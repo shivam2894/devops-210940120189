4e7dca2fd27244ea97ca7575482dfb17a26c2854
Shivam Tiwari <shivam2894@gmail.com>
2022-01-30 14:46:16 +0530

    Java code added

---
+++ Javacode.java
@@ -0,0 +1,14 @@
+class JavaCode 
+{
+
+	public static void main(String[] args) 
+	{
+		int a= Integer.parseInt(args[0]);
+		int b= Integer.parseInt(args[1]);
+		int c= Integer.parseInt(args[2]);
+		System.out.print("Average= " + ((a+b+c)/3));
+		
+	}
+	
+
+}
